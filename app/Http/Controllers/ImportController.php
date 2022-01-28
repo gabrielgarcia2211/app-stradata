2c39cb13f20ec3240f5f4a3326996bed4f1cfae7
gabriel <gabrielarturogq@ufps.edu.co>
2022-01-28 14:13:46 -0500

    Proceso: Importando CVS

---
+++ app/Http/Controllers/ImportController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ImportController extends Controller
+{
+    public function import()
+    {
+        Excel::load('books.csv', function($reader) {
+            foreach ($reader->get() as $book) {
+                Book::create([
+                    'name' => $book->title,
+                    'author' =>$book->author,
+                    'year' =>$book->publication_year
+                ]);
+            }
+        });
+        return Book::all();
+    }
+}
